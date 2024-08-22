fbdf1eebabd07bc8ba9670c02691e99c17247afe
wachira <wachirastudeo@gmail.com>
2024-08-22 22:19:27 +0700

    account ต่อ firebase

--- src/stores/account.js
+++ src/stores/account.js
@@ -1,6 +1,9 @@
 import { defineStore } from 'pinia';
-import { db, auth } from '@/firebase';
+// ทำการ import firestore เพิ่มเข้ามา
 import { doc, getDoc, setDoc } from 'firebase/firestore';
+
+// ทำการเพิ่ม db สำหรับเขียนข้อมูลเข้ามา
+import { db, auth } from '@/firebase';
 
 import {
     GoogleAuthProvider,
@@ -17,26 +20,84 @@ export const useAccountStore = defineStore('account', {
     state: () => ({
         isLoggedIn: false,
         isAdmin: false,
-        user: {}
+        user: {},
+        profile: {}
     }),
     actions: {
         async checkAuthState() {
             return new Promise((resolve) => {
-                onAuthStateChanged(auth, (user) => {
-                    if (user) {
-                        this.user = user;
-                        if (this.user.email === 'admin@gmail.com') {
-                            this.isAdmin = true;
-                        }
-                        this.isLoggedIn = true;
 
-                        resolve(true);
-                    } else {
+                onAuthStateChanged(auth, async (user) => {
+                    try {
+                        if (user) {
+                            this.user = user;
+                            this.isLoggedIn = true;
+                            // เพิ่มการอ้างอิงถึง collection users, document id 
+                            const docRef = doc(db, 'users', user.uid);
+                            console.log(docRef);
+                            const docSnap = await getDoc(docRef);
+
+                            // ถ้าข้อมูลไม่มีอยู่ = เขียนข้อมูลเข้าไปใหม่
+                            if (!docSnap.exists()) {
+                                const userData = {
+                                    name: user.displayName,
+                                    role: 'member',
+                                    status: 'active',
+                                    updatedAt: new Date()
+                                };
+                                await setDoc(docRef, userData);
+                                this.profile = userData;
+                            } else {
+                                this.profile = docSnap.data();
+                            }
+                            if (this.profile.role !== 'member') {
+                                this.isAdmin = true;
+                            }
+                            resolve(true);
+                        } else {
+                            resolve(false);
+                        }
+                    } catch (error) {
+                        console.log('error', error);
                         resolve(false);
                     }
                 });
             });
+
         },
+        // async checkAuthState() {
+        //     return new Promise((resolve) => {
+        //         onAuthStateChanged(auth, async (user) => {
+        //             try {
+        //                 if (user) {
+        //                     this.user = user;
+        //                     this.user = user;
+        //                     this.isLoggedIn = true;
+        //                     // เพิ่มการอ้างอิงถึง collection users, document id 
+        //                     const docRef = doc(db, 'users', user.uid);
+
+
+
+
+
+
+        //                     if (this.user.email === 'admin@gmail.com') {
+        //                         this.isAdmin = true;
+        //                     }
+        //                     this.isLoggedIn = true;
+        //                     // member first login create user
+
+        //                     resolve(true);
+        //                 } else {
+        //                     resolve(false);
+        //                 }
+        //             } catch (error) {
+
+        //             }
+
+        //         });
+        //     });
+        // },
         async signInWithGoogle() {
             try {
                 const result = await signInWithPopup(auth, provider);
